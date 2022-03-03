573cf6348897cb1310afa131cb70939a922d7cce
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2022-03-03 22:54:09 +0530

    Update Prob03.java

--- Prob03.java
+++ Prob03.java
@@ -3,13 +3,10 @@ public class Prob03 {
 
 	public static void main(String[] args) {
 		long n = 600851475143l;
-		for(int i=2; i*i<=n;i++)
-		{
+		for(int i=2; i*i<=n;i++) {
 			if(n%i==0) {
 				System.out.println(i);
 			}
 		}
-
 	}
-
 }
